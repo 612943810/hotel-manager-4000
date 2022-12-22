165836fe7e496b7a03082635fa1d752ae5c9f5be
Raiyan Rofiquzzaman <raiyanrofiquzzaman@hotmail.com>
2022-12-21 22:02:18 -0500

    This update createsa DbContext and Guest and Owner models.

---
+++ Hotel Manager 4000/Hotel Manager 4000/Data/HotelContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Hotel_Manager_4000.Data
+{
+    public class HotelContext:DbContext
+    {
+        public HotelContext(DbContextOptions<HotelContext> options)
+            :base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- Hotel Manager 4000/Hotel Manager 4000/Models/Owner.cs
+++ Hotel Manager 4000/Hotel Manager 4000/Models/Owner.cs
@@ -1,6 +1,11 @@
-﻿namespace Hotel_Manager_4000.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hotel_Manager_4000.Models
 {
     public class Owner
     {
+        [Key]
+        public int OwnerId { get; set; }
+        public string OwnerName { get; set; }
     }
 }
