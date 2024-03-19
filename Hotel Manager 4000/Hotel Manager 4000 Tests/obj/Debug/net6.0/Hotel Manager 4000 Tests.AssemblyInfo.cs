ba586b4a619dff6afb1939392b528f8bf839c742
612943810 <raiyanrofiquizzaman@gmail.com>
2024-03-19 01:40:35 -0400

    This update fixes issues with the controllers.

--- Hotel Manager 4000/Hotel Manager 4000 Tests/obj/Debug/net6.0/Hotel Manager 4000 Tests.AssemblyInfo.cs
+++ Hotel Manager 4000/Hotel Manager 4000 Tests/obj/Debug/net6.0/Hotel Manager 4000 Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hotel Manager 4000 Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34f59b5ec258b273b0a4f7b4325cb45931d47885")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+147d9cd249986fb2439c056bf997a2683332f986")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hotel Manager 4000 Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hotel Manager 4000 Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
